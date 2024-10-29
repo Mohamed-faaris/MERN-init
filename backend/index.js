9a34836517864fc64e3b7f2878ba8b957194a9a8
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2024-10-29 16:16:11 +0530

    added schema

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,28 @@ import mongoose from 'mongoose';
 dotenv.config({ path: './config/.env' }); 
 
 const mongoDatabaseURL = process.env.mongoDatabaseURL;
-const SERVER_PORT = process.env.SERVER_PORT; 
+const SERVER_PORT = process.env.SERVER_PORT
+
+const taskSchema = new mongoose.Schema({
+  key:{type:Number,default:()=>{Math.floor(Math.random()*100000)},index:true},
+  title:{type:String , required:true} ,
+  description:{type:String , required:true},
+  priority:{type:Number,default:5},
+  deadline:{type:Date},
+  createdAt:{type:Date,default:()=>{new Date()},index:true}
+})
+
+const userTaskState=mongoose.Schema({
+  Key:{type:Number,required:true},
+  completedAt:{type:Date,default:null},
+  seenAt:{type:Date,default:null},
+  thrownToBinAt:{type:Boolean,default:false}
+})
+  
 
 async function mongooseConnect() {
   try {
-    await mongoose.connect(mongoDatabaseURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoDatabaseURL);
     console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("Connection failed:", error);
