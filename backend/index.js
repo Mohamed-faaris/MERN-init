095a18220875e34ad033987fef5b58562b0ad33b
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2024-10-31 13:16:08 +0530

    added API endpoints

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,20 @@ import mongoose from 'mongoose';
 
 dotenv.config({ path: './config/.env' }); 
 
+const app = express();
+app.use(express.json());
+
 const mongoDatabaseURL = process.env.mongoDatabaseURL;
 const SERVER_PORT = process.env.SERVER_PORT
 
 const taskSchema = new mongoose.Schema({
-  key:{type:Number,default:()=>{Math.floor(Math.random()*100000)},index:true},
-  title:{type:String , required:true} ,
+  title:{type:String , required:true,unique:true} ,
   description:{type:String , required:true},
   priority:{type:Number,default:5},
-  deadline:{type:Date},
-  createdAt:{type:Date,default:()=>{new Date()},index:true}
+  deadline:{type:Date}
 })
+
+const Task = new mongoose.model("Task",taskSchema);
 
 const userTaskState=mongoose.Schema({
   Key:{type:Number,required:true},
@@ -27,22 +30,46 @@ const userTaskState=mongoose.Schema({
 async function mongooseConnect() {
   try {
     await mongoose.connect(mongoDatabaseURL);
-    console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("Connection failed:", error);
   }
 }
-const app = express();
 
 app.get("/", (req, res) => {
     res.send("connected");
   });
 
-app.get("/task", (req, res) => {
-    res.status(200).send("connected");
-  });
+app.get("/getTasks", async (req, res) => {
+  console.log("@getTask ");
+  res.status(200).json(await Task.find().sort({createdAt:-1}));
+});
+
+app.post("/createTestTask", async(req, res) => {
+  const task = new Task({
+    title:`task id-${Math.floor(Math.random()*10000)}`,
+    description:`test task created at ${new Date()}`,
+  })
+  try {
+    await task.save()
+    res.status(200).json({"status":"success","task":await Task.findOne().sort({ createdAt: -1 })});
+  } catch (error) {
+    res.status(400).json({"status":"failed","error" : error});
+  }
+});
+
+app.post("/createTask", async(req, res) => {
+  console.log("@createTasks ");
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(200).json({"status":"success","task":await Task.findOne().sort({ createdAt: -1 })});
+  } catch (error) {
+    res.status(400).json({"status":"failed", ...error});
+  }
+});
 
 app.listen(SERVER_PORT,async () => {
-    await mongooseConnect();  
-    console.log(`server is running on http://localhost:${SERVER_PORT}`);
+    console.log(`connecting to mongoDB....`);
+    await mongooseConnect(); 
+    console.log(`connected to mongoDB and server is running on http://localhost:${SERVER_PORT}`);
 });
